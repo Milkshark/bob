742abd56b5e4404fbe98e2cd87822dea8fd4fd96
Milkshark <maciek.wolski42@gmail.com>
2020-11-20 11:31:09 +0100

    Update bob.py

--- bob.py
+++ bob.py
@@ -1,6 +1,8 @@
 import random as r
 
 def correct(liste):
+	##Verity if a list is good
+	## 3 same digit in a row is bad
     suite = 0
     chiffre_de_suite = liste[0]
     for e in liste:
@@ -15,6 +17,7 @@ def correct(liste):
 
 
 def liste_up_low(n):
+	##Create a random arrangement for a word
     liste = []
     for i in range(n):
         liste.append(r.randint(0,1))
@@ -25,9 +28,10 @@ def liste_up_low(n):
         
  
 def mot_casse(mot):
+	##Randomise upper and lower case of a word
     mot_c = ''
     li = liste_up_low(len(mot))
-    li[0] = 1
+    li[0] = 1 #First letter of a word will always be lowered
     for i in range(len(mot)):
         if li[i] == 0:
             mot_c += mot[i].upper()
@@ -37,6 +41,7 @@ def mot_casse(mot):
     return mot_c
 
 def bob(phrase):
+	##Randomise upper and lower case of a sentence
     phrase_c = ''
     for word in phrase.split(' '):
         phrase_c += mot_casse(word) + ' '
